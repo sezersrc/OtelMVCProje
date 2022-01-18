100da242ae3884f9d974b1e878d3a0ae2bf42df0
Sezer Sürücü <sezer@sezersurucu.com>
2022-01-18 20:18:16 +0300

    tema edit

---
+++ Controllers/KayitController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMVCProje.Controllers
+{
+    public class KayitController : Controller
+    {
+        // GET: Kayit
+        public ActionResult KayitOl()
+        {
+            return View();
+        }
+    }
+}
