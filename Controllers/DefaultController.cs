65eea0365f3a944853c0fb33a19171d56e0ea8e7
Sezer Sürücü <sezer@sezersurucu.com>
2022-01-18 16:05:55 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+
+namespace OtelMVCProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+        public ActionResult Hakkimda()
+        {
+            return View();
+        }
+       
+
+        public ActionResult İletisim()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSosyalMedya()
+        {
+            return PartialView();
+        }
+
+    }
+}
