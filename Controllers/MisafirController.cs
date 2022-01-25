88da406ef3e3c14795c4911f65e706bd479c0357
Sezer Sürücü <sezer@sezersurucu.com>
2022-01-25 19:51:57 +0300

    Web Kayıt

--- Controllers/MisafirController.cs
+++ Controllers/MisafirController.cs
@@ -12,18 +12,30 @@ namespace OtelMVCProje.Controllers
         // GET: Misafir
 
         private DbOtelDevExEntities db = new DbOtelDevExEntities();
+       
         public ActionResult Index()
         {
-            return View();
+            var misafirmail = (string)Session["Mail"];
+            var misafirbilgi = db.TblWebKayit.Where(x => x.Mail == misafirmail).FirstOrDefault(); 
+            return View(misafirbilgi);
         }
 
         public ActionResult Rezervasyonlarim()
         {
             var misafirmail = (string)Session["Mail"];
-            ViewBag.deger = misafirmail;
-
-            var degerler = db.TblRezervasyon.Where(x => x.Misafir == 2).ToList();
+            var misafirid = db.TblWebKayit.Where(x => x.Mail == misafirmail).Select(y => y.ID).FirstOrDefault();
+            var degerler = db.TblRezervasyon.Where(x => x.Misafir == misafirid).ToList();
             return View(degerler);
+        }
+
+        public ActionResult MisafirBilgiGuncelle(TblWebKayit p)
+        {
+            var misafir = db.TblWebKayit.Find(p.ID);
+            misafir.AdSoyad = p.AdSoyad;
+            misafir.Telefon = p.Telefon;
+            misafir.Sifre = p.Sifre;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
